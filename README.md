f79ae856ea25d41f3d30804d35fc11c122072eff
Aaron <astrauli@gmail.com>
2017-04-11 17:39:57 -0700

    Add first method

---
+++ README.md
@@ -0,0 +1,1 @@
+The bubble_sort method uses the bubble sort algorithm to order an array from least to greatest.
---
+++ bubbleSort.rb
@@ -0,0 +1,22 @@
+def bubble_sort array
+  unsorted = true
+  while unsorted
+    unsorted = false
+    for i in (0..(array.length-1)) do
+      if i == 0
+      else
+        if array[i] < array[i-1]
+          current = array[i]
+          previous = array[i-1]
+          array[i-1] = current
+          array[i] = previous
+          unsorted = true
+        else
+        end
+      end
+    end
+  end
+  print array
+end
+
+bubble_sort([1,100, 1022, 1, 2, 10,9,3])
